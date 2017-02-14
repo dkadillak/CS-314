f53b4a6fd3975e8677fc6791368a2911d36a36d7
Peter Brunner <pjbrunn@earth.cs.colostate.edu>
2017-02-14 10:44:43 -0700

    closes #46

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -1,11 +1,14 @@
 package Presenter;
 
 import java.util.ArrayList;
+import Model.Model;
 import View.View;
 
 public class Presenter {
 	private View view;
-	public Presenter(View view) {
+	private Model model;
+	public Presenter(View view, Model model) {
 		this.view = view;
+		this.model = model;
 	}
 }
