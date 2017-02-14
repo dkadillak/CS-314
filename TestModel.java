9e0532864fd8fdcb3a0a77a203d83c5230e6b655
dkadillak <devbot@rams.colostate.edu>
2017-02-13 22:47:25 -0700

    closes #42

---
+++ TestModel.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestModel{
+
+
+	@Before
+	public void initialize(){
+	ArrayList<Integer> testArray= new ArrayList<Integer>(10);
+	Model m = new Model(10);
+	}
+
+
+	@Test
+	public void test(){
+
+		assertEquals(testArray.size(),m.getSize());
+
+	}
+
+}
