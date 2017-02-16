633d9b55516f23f0e3edc833321088980e1d7314
dkadillak <devbot@rams.colostate.edu>
2017-02-16 14:26:31 -0700

    Changes to Model

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,5 +1,13 @@
 package Model;
-
+//reader
+//inputstream reader
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -8,20 +16,107 @@ public class Model{
 	
 	//class variables
 	private ArrayList<location> data;
+	public ArrayList<String> extra;
+	public String Name;
+	public int ID;
+	private int lineCount=0, NamePosition, IDPosition, LatitudePosition, LongitudePosition;
+	public double Latitude, Longitude;
+	Scanner scan;
 
-	public Model(Stream file){
-		parseData(file);
+	public Model(File file) throws FileNotFoundException{
+	//setting up scanner with inputted file
+		scan= new Scanner(file);
+	//setting up scanner to parse individual tokens by comma
+		scan.useDelimiter(",");
+	//lesgo bby
+		parseData();
 	}
-	public int getSize(){
+	public int getFileSize(){
 		return data.size();
 	}
-	private void parseData(Stream file){
+	private void parseData(){
+		//setting up count
+				int count=0;
+		//setting up string 
+		String input="";
+		//getting information from very first line of file
+		if(scan.hasNextLine()){
+			firstLineParser(scan.nextLine());
+		}
+		
+	while(scan.hasNext()){
 		
 		
+		//removing all white space from a token
+		input =scan.next();
+		input=input.replaceAll("\\s", "");
+		
+		if(count==NamePosition){
+			Name=input;
+		}
+		else if(count==IDPosition){
+			ID=Integer.parseInt(input);
+		}
+		else if(count==LatitudePosition){
+			Latitude=LatLongConverter(input);
+		}
+		else if(count==LongitudePosition){
+			Longitude=LatLongConverter(input);
+		}
+		else{
+			extra.add(input);
+		}
 		
 		
-	
+		System.out.println(input);
+		count++;
+		}	
+	data.add(new location(Name,ID,Latitude,Longitude,extra));
+	extra.clear();
 	}
 
+protected void firstLineParser(String firstLine){
+	String input="";
+	//lowercase everything
+	firstLine=firstLine.toLowerCase();
+	
+
+	//remove extra spacing if there is any
+	firstLine.replaceAll("\\s", "");
+	
+	//setting up scanner for firstLine string and setting delimiter
+	Scanner in = new Scanner(firstLine);
+	in.useDelimiter(",");
+	
+	while(in.hasNext()){
+		input=in.next();
+		
+		if(input.equalsIgnoreCase("name")){
+			NamePosition=lineCount;
+		}
+		if(input.equalsIgnoreCase("id")){
+			IDPosition=lineCount;
+		}
+		if(input.equalsIgnoreCase("latitude")){
+			LatitudePosition=lineCount;
+		}
+		if(input.equalsIgnoreCase("longitude")){
+			LongitudePosition=lineCount;
+		}
+		lineCount++;
+	}
+	/* Testing to see if variables were set correctly
+	System.out.println("lineCount="+lineCount);
+	System.out.println("NamePosition="+NamePosition);
+	System.out.println("IDPosition="+IDPosition);
+	System.out.println("LatitudePosition="+LatitudePosition);
+	System.out.println("LongitudePosition="+LongitudePosition);
+	*/
+}
+//still need to implement
+private double LatLongConverter(String LatLong){
+	
+	return 2.12;
+}
 
 }
--- src/Model/location.java
+++ src/Model/location.java
@@ -1,13 +1,16 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class location {
 	
 	//class variables
-	private String fileType;
+	//private String fileType;
 	
-	private String name, quadrangle, countySeat, city;
-	private int id, elevation, estimatedProminence;
+	private String name; 
+	private int id;
 	private double latitude, longitude;
+	private ArrayList<String> other;
 	
 	//double getters
 	public double getLatitude(){
@@ -17,25 +20,27 @@ public class location {
 	public double getLongitudee(){
 		return longitude;
 	}
-	
 	//String getters
 	public String getName(){
 		return name;
 	}
 	
-	public String getQuadrangle(){
-		return quadrangle;
+	//generic constructor
+	public location(String Name, int ID, double Latitude, double Longitude, ArrayList<String> Other){
+		
+		name=Name;
+		id=ID;
+		latitude=Latitude;
+		longitude=Longitude;
+		other=Other;
+		
 	}
 	
-	public String getCountySeat(){
-		return countySeat;
-	}
-	
-	public String getCity(){
-		return city;
-	}
-	
-	//int getters
+	/* Unused constructors and variables
+	 String quadrangle, countySeat, city;
+	 int elevation, estimatedProminence;
+	  
+	int getters
 	public int getId(){
 		return id;
 	}
@@ -48,7 +53,17 @@ public class location {
 		return estimatedProminence;
 	}
 	
+		public String getQuadrangle(){
+		return quadrangle;
+	}
 	
+	public String getCountySeat(){
+		return countySeat;
+	}
+	
+	public String getCity(){
+		return city;
+	}
 	
 	//14ers location constructor
 	public location(String Name, int Id, int Elevation, int EstimatedProminence, double Latitude, double Longitude, String Quadrangle){
@@ -90,5 +105,5 @@ public class location {
 		longitude=Longitude;
 	}
 	
-	
+	*/
 }
