c4617c992396d1bfc75b476ba314c9339c7dde3f
pbrunner <pjbrunn@rams.colostate.edu>
2017-02-17 20:13:42 -0700

    Input will be parsed to make sure file passed in is .csv. Closes #61

--- src/TripCo.java
+++ src/TripCo.java
@@ -1,17 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
 import Model.Model;
 import View.View;
 import Presenter.Presenter;
 
 public class TripCo {
-	public static void main(String [ ] args)
-	{
-		ArrayList<Integer> id = null;
-		ArrayList<String> name = null;
-		ArrayList<Double> x = null, y = null;
-		View view = new View(id, name, x, y);
-		int rows = 0;
-		Model model = new Model(rows);
+	public static void main(String [ ] args) throws FileNotFoundException
+	{	
+		if(args.length != 0){ //check if there are any arguments at all
+			if(!args[0].substring(args[0].length() - 4).equals(".csv")){ //check if the file is type .csv or not
+				System.out.println("Error - File: " + "\"" + args[0] + "\"" + " is not of type .csv!");
+				System.exit(0);
+			}
+		}
+		else{
+			System.out.println("Error - No arguments given!");
+			System.exit(0);
+		}
+		String fileName = args[0].substring(0, args[0].length() - 4); //take ".csv" off the file name
+		File input = new File(args[0]);
+		File xml = new File(fileName + ".xml"); //make xml file with input file's name
+		File svg = new File(fileName + ".svg"); //make svg file with input file's name
+		View view = new View(xml, svg);
+		Model model = new Model(input);
 		Presenter presenter = new Presenter(view, model);
 	}	
 }
