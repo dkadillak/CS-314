db87deaba022d1f1ddedeae99bdfb42939cee9d6
Peter Brunner <pjbrunn@brazil.cs.colostate.edu>
2017-02-20 19:28:30 -0700

    Presenter now takes data from model and passes it to the view. Closes #81

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -1,6 +1,4 @@
 package Presenter;
-
-import java.util.ArrayList;
 import Model.Model;
 import View.View;
 
@@ -13,10 +11,42 @@ public class Presenter {
 	}
 	public void makeTrip(boolean opt_m, boolean opt_i, boolean opt_n){
 		for(int index = 0; index < model.legs.size(); index++){
-			view.addLeg(model.legs.get(index).getstartLocationID(), model.legs.get(index).getStartLocation(), model.legs.get(index).getendLocation(), model.legs.get(index).getDistane());
-			//System.out.println("i: " + index);
+			view.addLeg(model.legs.get(index).getstartLocationID(), model.legs.get(index).getStartLocation(), model.legs.get(index).getendLocation(), model.legs.get(index).getDistance());
 		}
-		
+		view.addHeader("Legs");
+		for(int index = 0; index < model.legs.size(); index++){
+			int [] first = view.convertCoords(model.locations.get(model.legs.get(index).getstartLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getstartLocationID()-1).getLongitude());
+			int [] second = view.convertCoords(model.locations.get(model.legs.get(index).getendLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getendLocationID()-1).getLongitude());
+			view.addLine(first[1], first[0], second[1], second[0]);
+		}
+		view.addFooter();
+		if(opt_m){
+			view.addHeader("Distances");
+			for(int index = 0; index < model.legs.size(); index++){
+				int [] first = view.convertCoords(model.locations.get(model.legs.get(index).getstartLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getstartLocationID()-1).getLongitude());
+				int [] second = view.convertCoords(model.locations.get(model.legs.get(index).getendLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getendLocationID()-1).getLongitude());
+				int midX = (first[1] + second[1]) / 2;
+				int midY = (first[0] + second[0]) / 2;
+				view.addLabel(midX, midY, Integer.toString(model.legs.get(index).getDistance()));
+			}
+			view.addFooter();
+		}
+		if(opt_i){
+			view.addHeader("Locations");
+			for(int index = 0; index < model.legs.size(); index++){
+				int [] point = view.convertCoords(model.locations.get(model.legs.get(index).getstartLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getstartLocationID()-1).getLongitude());
+				view.addLabel(point[1], point[0], Integer.toString(model.locations.get(model.legs.get(index).getstartLocationID()-1).getID()));
+			}
+			view.addFooter();
+		}
+		if(opt_n){
+			view.addHeader("Locations");
+			for(int index = 0; index < model.legs.size(); index++){
+				int [] point = view.convertCoords(model.locations.get(model.legs.get(index).getstartLocationID()-1).getLatitude(), model.locations.get(model.legs.get(index).getstartLocationID()-1).getLongitude());
+				view.addLabel(point[1], point[0], model.locations.get(model.legs.get(index).getstartLocationID()-1).getName());
+			}
+			view.addFooter();
+		}
 		view.finalizeTrip();
 	}
 }
