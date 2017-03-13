96caa827b3ef5f85d3eb946c63ea36db2fe55d78
dkadillak <devbot@rams.colostate.edu>
2017-03-12 19:15:30 -0600

    closes #96

--- src/test/java/edu/csu2017sp314/dtr18/TestTripCo.java
+++ src/test/java/edu/csu2017sp314/dtr18/TestTripCo.java
@@ -1,11 +1,20 @@
 package test.java.edu.csu2017sp314.dtr18;
 
 import static org.junit.Assert.*;
+import main.java.edu.csu2017sp314.dtr18.TripCo;
 
 import org.junit.Test;
 
 
 public class TestTripCo {
 
+	@Test
+	
+	public void testConstructor(){
+		int count = 4;
+		TripCo test = new TripCo(count);
+		assertEquals(count,test.getoptCount());
+	}
+ 
 
 }
